57333010a05871205cd176c24caec83f97dbf4a6
MatíasD1 <mati.durand98@gmail.com>
2023-11-22 15:45:42 -0300

    Clase 5

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
 //Componente Principal de la aplicación
-
+import ProfileCard from "./components/ProfileCard";
 import "./App.css"; 
-import ProfileCard from "./components/ProfileCard/ProfileCard";
-import AlexaImg from "./img/alexa.png";
-import CortanaImg from "./img/cortana.png";
-import SiriImg from "./img/siri.png";
 import "bulma/css/bulma.css";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-
+import AlexaImg from "./img/alexa.png";
+import SiriImg from "./img/siri.png";
+import CortanaImg from "./img/cortana.png";
 function App(){
         
     return (
         
-        <div >
+        <div>
           
             <section className="hero is-primary">
                 <div className="hero-body">
-                    <h1 className="title">Asistentes</h1>  
+                    <h1 className="title">E-commerce</h1>  
                     <NavBar />
                    
                 </div>
             </section>
             <ItemListContainer greeting={'Bienvenido'}/>
             <div className="container">
-          
-                <div className="columns">
+            <div className="columns">
                     <div className="column is-4">
-                        <ProfileCard titulo="Alexa" arroba="@Alexa22" img={AlexaImg}/>
+                        <ProfileCard titulo="Alexa" arroba="@Alexa22" img={AlexaImg}>
+                            <div>
+                                <h3>Mensaje enviado desde el padre</h3>
+                                <p>Texto......</p>
+                            </div>
+                        </ProfileCard>                    </div>
+                    <div className="column is-4">
+                        <ProfileCard titulo="Cortana" arroba="@Cortana66" img={CortanaImg}> 
+                            <div>
+                                <h3>Mensaje enviado desde el padre</h3>
+                                <p>Texto......</p>
+                            </div>
+                        </ProfileCard>                  
                     </div>
                     <div className="column is-4">
-                        <ProfileCard titulo="Cortana" arroba="@Cortana66" img={CortanaImg}/>
+                        <ProfileCard titulo="Siri" arroba="@Siri77" img={SiriImg}> 
+                            <div>
+                                <h3>Mensaje enviado desde el padre</h3>
+                                <p>Texto......</p>
+                            </div>
+                        </ProfileCard>                  
                     </div>
-                    <div className="column is-4">
-                        <ProfileCard titulo="Siri" arroba="@Siri77" img={SiriImg}/>
-                    </div>
+
                 </div>
             </div>   
         </div>
---
+++ src/components/ProfileCard.js
@@ -0,0 +1,63 @@
+import { useState } from "react"; //#1/5 UseState es una funcion que devuelve un arreglo
+
+function ProfileCard(props){
+    
+    const { titulo , arroba, img, children } = props;
+
+    const [count, setCount] = useState(0);//#2 UseState devuelve una variable y una funcion setter que carga la variable inicializada en este caso en 0 
+
+    function handleClickSuma(){/*Siempre es handle + nombre del evento*/  
+       //console.log("Click")
+        setCount(count+1);//#5 Incremento. Cada vez que incremento se vuelve a ejecutar todo el componente sumando 1
+
+    }
+    function handleClickResta(){
+     setCount(count-1);
+ }
+    return (/*State es la unica propiedad que permite cambiar el contenido que React muestra */
+
+        <div className="card">
+            <div className="card-img">
+ 
+                <figure className="image is-1by1">
+                    <img src={img} alt="logo"/>
+                </figure>
+            </div>           
+            <div className="card-content">
+                <div className="media-content">
+                    <h3 className="title is-4">Titulo: {titulo}</h3>
+                    <p className="subtitle is-4">{arroba}</p>
+                </div>
+
+                <div className="columns">
+                    
+                    <div className="column is-4">
+                    
+                        <button onClick={handleClickSuma} className="button is-primary full-width is-fullwidth">
+                            <p>+</p>
+                        </button> {/*#4 Cuando se escucha el click, se ejecuta la funcion {handleClickSuma} */} 
+                        
+                        </div>
+
+                            <div className="column">
+                                <p>Likes: {count}</p>{/*#3 Utilizo la variable de estado*/}
+                            </div>
+
+                        <div className="column">
+                        <button onClick={handleClickResta} className="button is-danger is-fullwidth">
+                            <p>-</p>
+                        </button>  
+                    </div>
+                    
+                </div>
+
+                {children}
+                
+            </div>
+            
+        </div>
+    )
+
+}
+
+export default ProfileCard;
